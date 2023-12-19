65f2b3dfd6efff25bb5a68080c09f7a60990b33b
swJANG <123090529+swJaNG12@users.noreply.github.com>
2023-12-19 19:38:19 +0900

    Time: 123 ms (34.57%), Space: 49.9 MB (32.10%) - LeetHub

---
+++ LeetCode/Easy/0661-image-smoother/0661-image-smoother.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} img
+ * @return {number[][]}
+ */
+
+ /**
+ [0,0] -> [0,0], [0,1], [1,0], [1,1]
+ [0,1] -> [0,0], [0,1]. [0,2], [1,0], [1,1], [1,2]
+ [1,0] -> [0,0], [1,0], [2,0], [1,1], 
+ 0,1,2  | 0,1
+
+ [2,2]
+ 1,2  |  1,2
+
+ [1,2]
+ 0,1,2 | 1,2
+
+ [0,0]
+ 0,1 | 0,1
+  */
+var imageSmoother = function(img) {
+  const m = img.length;
+  const n = img[0].length;
+  const result = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  const directions = [[-1,-1], [-1,0], [-1, 1], [0,-1], [0, 1], [1,-1], [1,0], [1,1]]
+
+  for(let i = 0; i < m; i++) {
+    for(let j = 0; j < n; j++) {
+
+      let sum = img[i][j];
+      let count = 1;
+      for(let [dx, dy] of directions) {
+        const x = i + dx, y = j + dy;
+        if(x >= 0 && x < m && y >= 0 && y < n) {
+          sum += img[x][y];
+          count++;
+        }
+      }
+      result[i][j] = Math.floor(sum / count);
+    }
+  }
+  return result;
+};
