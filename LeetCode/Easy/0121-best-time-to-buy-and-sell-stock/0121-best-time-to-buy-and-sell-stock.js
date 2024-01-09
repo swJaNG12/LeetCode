06c25b9c19af9a222365cf8f4d437d692a97b38a
swJANG <123090529+swJaNG12@users.noreply.github.com>
2024-01-09 14:59:55 +0900

    Time: 68 ms (79.15%), Space: 51.7 MB (31.50%) - LeetHub

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -7,7 +7,6 @@ var maxProfit = function (prices) {
   let min = prices[0];
   let maxProfit = 0;
 
-  const profitArray = []
   for (let i = 1; i < prices.length; i++) {
     if (prices[i] < min) min = prices[i];
     if(maxProfit < prices[i] - min) {
