7d30d015ab0fd3aea31f9be7040e1eb24a8ca99b
swJANG <123090529+swJaNG12@users.noreply.github.com>
2024-01-16 15:42:44 +0900

    Time: 71 ms (27.64%), Space: 44.4 MB (88.25%) - LeetHub

---
+++ LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+  if(head === null) return false; 
+
+  let slow = head;
+  let fast = slow.next;
+
+  while(fast !== null && fast.next !== null) {
+    if(slow === fast) return true;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return false;
+};
