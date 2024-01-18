49912867149b93c56bc64751ace4655bf06d6d32
swJANG <123090529+swJaNG12@users.noreply.github.com>
2024-01-18 18:14:29 +0900

    Time: 74 ms (74.89%), Space: 50.1 MB (40.72%) - LeetHub

--- LeetCode/Easy/0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ LeetCode/Easy/0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -12,15 +12,10 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-  const set = new Set();
-  while(headA !== null) {
-    set.add(headA);
-    headA = headA.next;
+  let p1 = headA, p2 = headB;
+  while(p1 !== p2) {
+    p1 = p1 === null ? headB : p1.next;
+    p2 = p2 === null ? headA : p2.next;
   }
-
-  while(headB !== null) {
-    if(set.has(headB)) return headB;
-    headB = headB.next;
-  }
-  return null;
+  return p1;
 };
