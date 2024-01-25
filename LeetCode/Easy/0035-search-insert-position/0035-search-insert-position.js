267f9ac8c0b7944c97ac54a946eca736d036a171
swJANG <123090529+swJaNG12@users.noreply.github.com>
2024-01-25 21:12:20 +0900

    Time: 50 ms (71.09%), Space: 49.2 MB (5.55%) - LeetHub

---
+++ LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  let left = 0, right = nums.length - 1
+  
+  while(left <= right) {
+    const mid = Math.floor(left + right);
+    if(nums[mid] === target) return mid;
+
+    if(nums[mid] < target) left = mid + 1;
+    else right = mid - 1; 
+  }
+  return left;
+};
