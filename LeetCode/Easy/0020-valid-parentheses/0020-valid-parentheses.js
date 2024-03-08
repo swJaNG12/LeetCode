dd6d4d99a14ba25d74215c6009c358f600717330
swJANG <123090529+swJaNG12@users.noreply.github.com>
2024-03-08 20:46:57 +0900

    Time: 58 ms (53.28%), Space: 50.5 MB (30.66%) - LeetHub

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.js
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.js
@@ -7,18 +7,20 @@
 var isValid = function (s) {
   const stack = [];
   const brackets = new Map([
-    [')', '('],
-    ['}', '{'],
-    [']', '[']
+    ['(', ')'],
+    ['{', '}'],
+    ['[', ']']
   ])
 
-  for(let i = 0; i < s.length; i++) {
-    if(brackets.has(s[i])) {
-      if(stack.length === 0) return false;
-      if(stack.pop() !== brackets.get(s[i])) return false;
-      
-    } else stack.push(s[i])
+  for (let i = 0; i < s.length; i++) {
+    if (!brackets.has(s[i])) {
+      if (stack.length === 0) return false;
+      if (brackets.get(stack[stack.length - 1]) !== s[i]) return false;
+      stack.pop();
+    } else {
+      stack.push(s[i]);
+    }
   }
 
-  return stack.length === 0;
+  return stack.length === 0 ? true : false;
 };
