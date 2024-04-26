0f340595bd4640548a6ee03b0e1f775d82759235
swJANG <123090529+swJaNG12@users.noreply.github.com>
2024-04-26 18:37:52 +0900

    Time: 63 ms (96.73%), Space: 51.5 MB (99.56%) - LeetHub

--- LeetCode/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
+++ LeetCode/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -3,8 +3,32 @@
  * @return {number}
  */
 var maximumProduct = function (nums) {
-    nums.sort((a,b) => a-b);
-    let max1 = nums[0] * nums[1] * nums[nums.length - 1];
-    let max2 = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3]
-    return max1 >= max2 ? max1 : max2;
+  let min1 = Number.MAX_SAFE_INTEGER, min2 = Number.MAX_SAFE_INTEGER;
+  let max1 = Number.MIN_SAFE_INTEGER, max2 = Number.MIN_SAFE_INTEGER, max3 = Number.MIN_SAFE_INTEGER;
+
+  for (let i = 0; i < nums.length; i++) {
+    let currnetNum = nums[i];
+
+    if (currnetNum <= min1) {
+      min2 = min1;
+      min1 = currnetNum;
+    } else if (currnetNum <= min2) {
+      min2 = currnetNum;
+    }
+
+    if (currnetNum >= max1) {
+      max3 = max2;
+      max2 = max1;
+      max1 = currnetNum;
+    } else if (currnetNum >= max2) {
+      max3 = max2;
+      max2 = currnetNum;
+    } else if (currnetNum >= max3) {
+      max3 = currnetNum;
+    }
+  }
+  const CASE_1 = min1 * min2 * max1;
+  const CASE_2 = max1 * max2 * max3;
+
+  return CASE_1 >= CASE_2 ? CASE_1 : CASE_2;
 };
